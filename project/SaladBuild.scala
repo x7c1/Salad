5106ba7a1b266cff79a33c8fc497b47daae8ac43
x7c1 <jmtaro+x7c1@gmail.com>
2014-11-21 23:57:43 +0900

    refactor project definitions

---
+++ project/SaladBuild.scala
@@ -0,0 +1,29 @@
+import sbt._
+import sbt.Keys._
+
+object SaladBuild extends Build {
+
+  val saladSettings = Seq(
+    scalaVersion := "2.11.2",
+    scalacOptions ++= Seq(
+      "-deprecation",
+      "-feature")
+  )
+
+  lazy val `salad-lib` = project.
+    settings(saladSettings:_*).
+    settings(libraryDependencies ++= Seq(
+      "org.scala-lang" % "scala-reflect" % scalaVersion.value
+    ))
+
+  lazy val `salad-sample` = project.
+    settings(saladSettings:_*).
+    settings(libraryDependencies ++= Seq(
+      "org.specs2" %% "specs2" % "2.3.13" % Test
+    )).
+    dependsOn(`salad-lib`)
+
+  lazy val root = Project("noir", file(".")).
+    aggregate(`salad-lib`, `salad-sample`)
+
+}
