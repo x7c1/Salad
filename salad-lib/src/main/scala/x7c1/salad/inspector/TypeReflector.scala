d30d586107813e6a53ae2d5139e7ae53e2a4c548
x7c1 <jmtaro+x7c1@gmail.com>
2014-11-30 00:43:21 +0900

    add parameter to filter symbol name

--- salad-lib/src/main/scala/x7c1/salad/inspector/TypeReflector.scala
+++ salad-lib/src/main/scala/x7c1/salad/inspector/TypeReflector.scala
@@ -4,6 +4,12 @@ import scala.reflect.runtime.universe._
 
 object TypeReflector {
   def inspect[A: WeakTypeTag]: SaladType = {
+    val defaultFilter = ! (_: String).startsWith("scala.")
+    new TypeReflector(defaultFilter).inspect[A]
+  }
+}
+class TypeReflector(nameFilter: String => Boolean){
+  def inspect[A: WeakTypeTag]: SaladType = {
     val target = implicitly[WeakTypeTag[A]]
 
     def findPackage(symbol: Symbol) =
@@ -11,8 +17,8 @@ object TypeReflector {
 
     def buildFrom(target: Type): SaladType = {
       val fields = target.members.view.
+        filter(x => nameFilter(x.owner.fullName)).
         filter(x => x.isMethod && x.isAbstract).
-        filter(! _.owner.fullName.startsWith("scala.")).
         map(_.asMethod).filter(_.paramLists.isEmpty).
         map{ method =>
           val resultType =  method.typeSignatureIn(target).resultType
