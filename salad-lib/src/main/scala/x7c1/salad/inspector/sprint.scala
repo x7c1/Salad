f6a844b2d43ef8918aadb9452d97b69eaf04b8f3
x7c1 <jmtaro+x7c1@gmail.com>
2014-11-30 11:48:01 +0900

    rename

--- salad-lib/src/main/scala/x7c1/salad/inspector/sprint.scala
+++ salad-lib/src/main/scala/x7c1/salad/inspector/sprint.scala
@@ -3,14 +3,14 @@ package x7c1.salad.inspector
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 
-object printLn {
-  def apply[A](x: => A): Unit = macro printLnImpl.impl[A]
+object sprint {
+  def apply[A](x: => A): Unit = macro sprintImpl.impl[A]
 }
 
-private object printLnImpl {
+private object sprintImpl {
   def impl[A: c.WeakTypeTag](c: blackbox.Context)(x: c.Expr[A]) = {
     import c.universe._
-    val source = show(x.tree)
+    val source = showCode(x.tree)
     val line = s"L${x.tree.pos.line}: $source => "
     q"""println($line + ${x.tree})"""
   }
