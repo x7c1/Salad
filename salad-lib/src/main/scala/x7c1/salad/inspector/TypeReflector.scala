24c24aa5152400463e164c000e5dc519c3dac8f3
x7c1 <jmtaro+x7c1@gmail.com>
2014-12-07 19:46:53 +0900

    refactor

--- salad-lib/src/main/scala/x7c1/salad/inspector/TypeReflector.scala
+++ salad-lib/src/main/scala/x7c1/salad/inspector/TypeReflector.scala
@@ -8,31 +8,11 @@ object TypeReflector {
     new TypeReflector(defaultFilter).inspect[A]
   }
 }
-class TypeReflector(nameFilter: String => Boolean){
+class TypeReflector(
+  val nameFilter: String => Boolean) extends TypeDigestFactory {
+
   def inspect[A: WeakTypeTag]: TypeDigest = {
     val target = implicitly[WeakTypeTag[A]]
-
-    def findPackage(symbol: Symbol) =
-      if (symbol.isPackage) Some(symbol.fullName) else None
-
-    def buildFrom(target: Type): TypeDigest = {
-      val fields = target.members.view.
-        filter(x => nameFilter(x.owner.fullName)).
-        filter(x => x.isMethod && x.isAbstract).
-        map(_.asMethod).filter(_.paramLists.isEmpty).
-        map{ method =>
-          val resultType =  method.typeSignatureIn(target).resultType
-          new FieldSummary(
-            decodedName = method.name.decodedName.toString,
-            resultType = buildFrom(resultType))
-        }
-
-      new TypeDigest(
-        packageName = findPackage(target.typeSymbol.owner),
-        fullName = target.typeSymbol.fullName,
-        typeArguments = target.typeArgs.map(buildFrom),
-        members = fields.toList )
-    }
-    buildFrom(target.tpe)
+    buildDigestFrom(target.tpe)
   }
 }
